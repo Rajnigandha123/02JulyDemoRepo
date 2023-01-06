52bc5d93b7207e3bf4a673fc9cb3e9449826ddb0
Rajnigandha123 <46079057+Rajnigandha123@users.noreply.github.com>
2023-01-06 13:24:48 +0530

    swapping
    hi team
    
    swapping two num done plx check

---
+++ PRISM_A/src/main/java/LoginPageLayer.java
@@ -0,0 +1,17 @@
+
+public class LoginPageLayer {
+
+	public static void main(String arg[])
+	{
+		int a=20,b=30,c;
+		
+		System.out.println("before swapping a="+ a+"  b="+b);
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("after swapping a="+ a+" b= "+b);
+		
+	}
+	
+	
+}
---
+++ PRISM_A/src/test/java/com/test/layer/LoginTestLayer.java
@@ -0,0 +1,5 @@
+package com.test.layer;
+
+public class LoginTestLayer {
+
+}
